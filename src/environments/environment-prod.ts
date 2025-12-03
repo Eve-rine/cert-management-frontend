3a377ca1df464434f124c8849d8e526ddcc77db2
evrine <minayoevrine@gmail.com>
2025-12-03 16:12:02 +0300

    deploy to netfly

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
 //   apiUrl: 'http://localhost:8080', 
-  apiUrl: process.env['API_URL'] || 'https://certificate-generation-backend-production.up.railway.app'
+  apiUrl:'https://certificate-generation-backend-production.up.railway.app'
 };
